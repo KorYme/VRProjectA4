993d2272fba2230d68db5a63fa5ea366537c7545
Orcoro <45428006+Orcoro@users.noreply.github.com>
2024-12-04 22:51:20 +0100

    send glowstick

---
+++ Source/VRProjectA4/Private/Glowstick.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRProjectA4/Public/Glowstick.h"
+
+
+// Sets default values
+AGlowstick::AGlowstick()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+// Called when the game starts or when spawned
+void AGlowstick::BeginPlay()
+{
+	Super::BeginPlay();
+	GetComponents(GrabComponents);
+}
+
+// Called every frame
+void AGlowstick::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AGlowstick::Grabbed()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, TEXT("Grabbed"));
+	PrimaryActorTick.bCanEverTick = true;
+	IsGrabbed = true;
+}
+
+bool AGlowstick::CheckIsCracked()
+{
+	return true;
+}
+
+void AGlowstick::Cracked()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Magenta, TEXT("Cracked"));
+	IsCracked = true;
+}
+
+void AGlowstick::CustomGrab_Implementation()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("CustomGrab_Implementation"));
+	Grabbed();
+}
+
---
+++ Source/VRProjectA4/Public/Glowstick.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Glowstick.generated.h"
+
+UCLASS()
+class VRPROJECTA4_API AGlowstick : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AGlowstick();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY()
+	bool IsGrabbed = false;
+
+	UPROPERTY()
+	bool IsCracked = false;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Glowstick")
+	float MaxIntensity = 100.f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Glowstick")
+	float TimeToMaxIntensity = 5.f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Glowstick")
+	UMaterial *GlowstickMaterial;	
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<TObjectPtr<USceneComponent>> GrabComponents;
+
+	UFUNCTION(BlueprintCallable, Category = "Glowstick")
+	void Grabbed();
+
+	UFUNCTION(BlueprintCallable, Category = "Glowstick")
+	bool CheckIsCracked();
+	
+	UFUNCTION(BlueprintCallable, Category = "Glowstick")
+	void Cracked();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Glowstick")
+	void CustomGrab();
+};
