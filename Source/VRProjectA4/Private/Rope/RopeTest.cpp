8094d8d278f2344de58491a07ddd6c192736f2c3
LeoLelann <112940420+LeoLelann@users.noreply.github.com>
2024-12-06 09:59:37 +0100

    Commit merge

---
+++ Source/VRProjectA4/Private/Rope/RopeTest.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRProjectA4/Public/Rope/RopeTest.h"
+
+
+// Sets default values
+ARopeTest::ARopeTest()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ARopeTest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARopeTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
